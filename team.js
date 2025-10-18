3f5f9ea88c9a695976e511cb4faa96dd33f1d797
Evgeniy Sych <evgenius007007@gmail.com>
2025-10-18 15:15:18 +0300

    Добавить информацию об участнике

--- team.js
+++ team.js
@@ -1,83 +1,87 @@
 export const team = [
-    {
-        "id": 1, 
-        "firstName": "Виталий",
-        "lastName": "Климко",
-        "age": 28,
-        "about": "Люблю фронтенд, участвовал в разработке UI и интеграции с API.",
-        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU9-uXk20n_g6_Qwl8G_Poxjd6PSPD-vumcw&s",
-        "social": {
-        "github": "https://github.com/Vitali_lab",
-        "telegram": "",
-        "instagram": ""
-        },
-        "role": "Инициализировал проект, реализовал Navbar, главную страницу, сетку и список карточек.",
-        "badge": "Team Lead",   
-        "skills": [
-        { "name": "React", "level": 80 },
-        { "name": "Node.js", "level": 60 },
-        { "name": "CSS", "level": 90 }
-        ]
-        },
-        {
-            "id": 2, 
-            "firstName": "Иван",
-            "lastName": "Иванов",
-            "age": 28,
-            "about": "Люблю фронтенд, участвовал в разработке UI и интеграции с API.",
-            "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU9-uXk20n_g6_Qwl8G_Poxjd6PSPD-vumcw&s",
-            "social": {
-            "github": "https://github.com/Vitali_lab",
-            "telegram": "",
-            "instagram": ""
-            },
-            "role": "чем занимался в проекте",
-            "badge": "Team Lead",   
-            "skills": [
-            { "name": "React", "level": 80 },
-            { "name": "Node.js", "level": 60 },
-            { "name": "CSS", "level": 90 }
-            ]
-            },
-            {
-                "id": 3, 
-                "firstName": "Петр",
-                "lastName": "Петров",
-                "age": 28,
-                "about": "Люблю фронтенд, участвовал в разработке UI и интеграции с API.",
-                "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU9-uXk20n_g6_Qwl8G_Poxjd6PSPD-vumcw&s",
-                "social": {
-                "github": "https://github.com/Vitali_lab",
-                "telegram": "",
-                "instagram": ""
-                },
-                "role": "чем занимался в проекте",
-                "badge": "Team Lead",   
-                "skills": [
-                { "name": "React", "level": 80 },
-                { "name": "Node.js", "level": 60 },
-                { "name": "CSS", "level": 90 }
-                ]
-                },
-                {
-                    "id": 4, 
-                    "firstName": "Днеис",
-                    "lastName": "Денисов",
-                    "age": 28,
-                    "about": "Люблю фронтенд, участвовал в разработке UI и интеграции с API.",
-                    "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU9-uXk20n_g6_Qwl8G_Poxjd6PSPD-vumcw&s",
-                    "social": {
-                    "github": "https://github.com/Vitali_lab",
-                    "telegram": "",
-                    "instagram": ""
-                    },
-                    "role": "чем занимался в проекте",
-                    "badge": "Team Lead",   
-                    "skills": [
-                    { "name": "React", "level": 80 },
-                    { "name": "Node.js", "level": 60 },
-                    { "name": "CSS", "level": 90 }
-                    ]
-                    },
-
-]
+  {
+    id: 1,
+    firstName: 'Виталий',
+    lastName: 'Климко',
+    age: 28,
+    about: 'Люблю фронтенд, участвовал в разработке UI и интеграции с API.',
+    photo:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU9-uXk20n_g6_Qwl8G_Poxjd6PSPD-vumcw&s',
+    social: {
+      github: 'https://github.com/Vitali_lab',
+      telegram: '',
+      instagram: '',
+    },
+    role: 'Инициализировал проект, реализовал Navbar, главную страницу, сетку и список карточек.',
+    badge: 'Team Lead',
+    skills: [
+      { name: 'React', level: 80 },
+      { name: 'Node.js', level: 60 },
+      { name: 'CSS', level: 90 },
+    ],
+  },
+  {
+    id: 2,
+    firstName: 'Иван',
+    lastName: 'Иванов',
+    age: 28,
+    about: 'Люблю фронтенд, участвовал в разработке UI и интеграции с API.',
+    photo:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU9-uXk20n_g6_Qwl8G_Poxjd6PSPD-vumcw&s',
+    social: {
+      github: 'https://github.com/Vitali_lab',
+      telegram: '',
+      instagram: '',
+    },
+    role: 'чем занимался в проекте',
+    badge: 'Team Lead',
+    skills: [
+      { name: 'React', level: 80 },
+      { name: 'Node.js', level: 60 },
+      { name: 'CSS', level: 90 },
+    ],
+  },
+  {
+    id: 3,
+    firstName: 'Петр',
+    lastName: 'Петров',
+    age: 28,
+    about: 'Люблю фронтенд, участвовал в разработке UI и интеграции с API.',
+    photo:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU9-uXk20n_g6_Qwl8G_Poxjd6PSPD-vumcw&s',
+    social: {
+      github: 'https://github.com/Vitali_lab',
+      telegram: '',
+      instagram: '',
+    },
+    role: 'чем занимался в проекте',
+    badge: 'Team Lead',
+    skills: [
+      { name: 'React', level: 80 },
+      { name: 'Node.js', level: 60 },
+      { name: 'CSS', level: 90 },
+    ],
+  },
+  {
+    id: 4,
+    firstName: 'Евгений',
+    lastName: 'Сыч',
+    age: 34,
+    about:
+      'Специализируюсь на frontend-разработке: создание интерфейсов и их взаимодействие с серверной частью через API.',
+    photo:
+      'https://dogtowndogtraining.com/wp-content/uploads/2012/06/300x300-061-e1340955308953.jpg',
+    social: {
+      github: 'https://github.com/FrontEvEndJenia',
+      telegram: '',
+      instagram: '',
+    },
+    role: 'Реализовал компонент Button, модальные окна добавления в избранное, удаления из избранного, типизировал приложение.',
+    badge: '',
+    skills: [
+      { name: 'React', level: 80 },
+      { name: 'Node.js', level: 70 },
+      { name: 'CSS', level: 60 },
+    ],
+  },
+];
